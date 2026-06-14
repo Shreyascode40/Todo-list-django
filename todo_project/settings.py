e005bb669b3b9ebe59e1588baea4a38431975734
Shreyascode40 <shreyasvmore34@gmail.com>
2026-06-14 12:36:22 +0530

    Fix LOGIN_URL to match actual login path (/account/login/)

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -139,6 +139,6 @@ STORAGES = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/account/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/account/login/'
